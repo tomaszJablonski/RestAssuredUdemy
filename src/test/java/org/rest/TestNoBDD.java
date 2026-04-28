8c7a3c51cad1ca252fd334afa8829829834ee4bc
tomasz.jablonski <tjablonski1990@gmail.com>
2026-04-28 22:42:39 +0200

    NoBDD

--- src/test/java/org/rest/TestNoBDD.java
+++ src/test/java/org/rest/TestNoBDD.java
@@ -1,40 +1,55 @@
 package org.rest;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.QueryableRequestSpecification;
+import io.restassured.specification.SpecificationQuerier;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.with;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class TestNoBDD {
 
-    RequestSpecification requestSpecification;
-
     @BeforeClass
     public void beforeClass() {
-        requestSpecification = with().
-                baseUri("https://api.postman.com/").
-                header("x-api-key", "PMAK-69d388a18c35aa0001781056-88fc76b581cad50a170fea601e6e7deec6")
-                .log().all();
+//        requestSpecification = with().
+//                baseUri("https://api.postman.com/").
+//                header("x-api-key", "PMAK-69d388a18c35aa0001781056-88fc76b581cad50a170fea601e6e7deec6")
+//                .log().all();
+
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder()
+                .setBaseUri("https://api.postman.com/")
+                .addHeader("x-api-key", "PMAK-69d388a18c35aa0001781056-88fc76b581cad50a170fea601e6e7deec6")
+                .log(LogDetail.ALL);
+
+        RestAssured.requestSpecification = requestSpecBuilder.build();
     }
 
+    @Test
+    public void queryTest(){
+        QueryableRequestSpecification queryableRequestSpecification = SpecificationQuerier.query(RestAssured.requestSpecification);
+        System.out.println(queryableRequestSpecification.getBaseUri());
+        System.out.println(queryableRequestSpecification.getHeaders());
+    }
 
     @Test
     public void testOne() {
-        Response response = requestSpecification.get("/workspaces")
-                        .then()
-                                .log().all()
-                        .extract().response();
+        Response response = get("/workspaces")
+                .then()
+                .log().all()
+                .extract().response();
         assertThat(response.statusCode(), is(equalTo(200)));
     }
 
     @Test
     public void testTwo() {
-        Response response = requestSpecification.get("/workspaces")
+        Response response = get("/workspaces")
                 .then()
                 .log().all()
                 .extract().response();
